d39f65b89de8381af4af1a9c71ae5a6587c4dc39
Twallacevtdi <146570081+Twallacevtdi@users.noreply.github.com>
2023-12-17 00:39:21 -0500

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,23 @@
+
+
+
+    <div id="footer" class="p-3 bg-primary text-white fixed-bottom">
+        <p class="text-center">Copyright &copy; - HCBS Assessment 2023 <?php echo date('Y'); ?></p>
+    </div>
+
+</div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.6.0.js"></script>
+<script src="https://code.jquery.com/ui/1.13.2/jquery-ui.js"></script>
+<script>
+  $( function() {
+    $( "#dob" ).datepicker({
+      changeMonth: true,
+      changeYear: true,
+      yearRange: "-100:+0",
+      dateFormat: "yy-mm-dd"
+    });
+  } );
+  </script>
+</body>
+</html>
---
+++ includes/header.php
@@ -0,0 +1,58 @@
+<?php
+include_once 'includes/session.php' 
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="//code.jquery.com/ui/1.13.2/themes/base/jquery-ui.css">
+    <link rel="stylesheet" href="css/style.css"/>
+    <title>Attendance - <?php echo $title ?></title>
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+  <div class="container-fluid">
+  <a class="navbar-brand" href="index.php">
+  <img src="logo.jpg" alt="" width="60" height="40" class="d-inline-block align-text-top">
+     
+
+
+    <a class="navbar-brand" href="index.php">HCBS Assessment</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="viewrecords.php">View Attendees</a>
+          
+        </li>
+
+        <li class="nav-item">
+        <a class="nav-link" href="about.php">About Us</a>
+        </li>
+
+
+      </ul>
+    </div>
+    <div class="navbar-nav ml-auto">
+          <?php 
+              if(!isset($_SESSION['userid'])){
+          ?>
+            <a class="nav-item nav-link" href="login.php">Login <span class="sr-only"></span></a>
+          <?php } else { ?>
+            <a class="nav-item nav-link" href="#"><span>Hello <?php echo $_SESSION['username'] ?>! </span> <span class="sr-only">(current)</span></a>
+            <a class="nav-item nav-link" href="logout.php">Logout <span class="sr-only">(current)</span></a>
+          <?php } ?>
+        </div>
+  </div>
+</nav>
+<div class="container">
+<br/>
+    
